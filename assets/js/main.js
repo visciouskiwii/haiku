8c554d7d8422b96dc44baa21c2a71c8c2c8d619c
visciouskiwii <lexiebecerra@gmail.com>
2026-04-06 23:09:29 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,20 @@
 // main.js
+let moved = false;
 
+document.addEventListener("click", function () {
+  let words = document.querySelectorAll(".grid-container div");
+
+  moved = !moved;
+
+  words.forEach(function (word) {
+    if (moved) {
+      let x = Math.random() * 200 - 100;
+      let y = Math.random() * 200 - 100;
+
+      word.style.transform = `translate(${x}px, ${y}px)`;
+    } else {
+      word.style.transform = `translate(0px, 0px)`;
+    }
+  });
+});
 console.log("Hello, World!");

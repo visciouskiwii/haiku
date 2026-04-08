9c88c00fa88dcdfbba7b257ae6eec4cb324190c4
visciouskiwii <lexiebecerra@gmail.com>
2026-04-07 19:20:47 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,3 @@
 // main.js
 
-let moved = false;
-
-document.addEventListener("click", function () {
-  let words = document.querySelectorAll(".grid-container div");
-
-  moved = !moved;
-
-  words.forEach(function (word, index) {
-    if (moved) {
-      let x = index * 8;
-      let y = -30 - (index * 5);
-
-      word.style.transform = `translate(${x}px, ${y}px)`;
-    } else {
-      word.style.transform = `translate(0px, 0px)`;
-    }
-  });
-});
 console.log("Hello, World!");

be90da15fa2d251294db8885dd65468bfa7dfc1b
visciouskiwii <lexiebecerra@gmail.com>
2026-04-07 19:38:55 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,20 @@
 // main.js
 
-let rotation = 0;
-setInterval(function () {
-  rotation += 5;
-  document.body.style.transform = `rotate(${rotation}deg)`;
-}, 360);
 
+document.addEventListener("click", function () {
+  let words = document.querySelectorAll(".grid-container div");
+
+
+  words.forEach(function (word, index) {
+    if (moved) {
+      let x = index * 8;
+      let y = -30 - (index * 5);
+
+      word.style.transform = `translate(${x}px, ${y}px)`;
+    } else {
+      word.style.transform = `translate(0px, 0px)`;
+    }
+  });
+});
 console.log("Hello, World!");
+
